f170e9a9e7197f17bdd099e388e36ccdc5d8b319
Ashley Hodge <ashley@Ashleys-MacBook-Pro-2.local>
2022-05-02 15:51:43 -0400

    create gallery and photolist tests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { useState } from 'react';
+import Nav from './components/Nav';
 import About from './components/About';
-import Nav from './components/Nav'
 import Gallery from './components/Gallery';
 
-import './App.css';
-
 function App() {
-  const [categories] = useState ([
+  const [categories] = useState([
     {
-      name: 'Commercial',
-      description:
-      'Photos of grocery stores, food trucks, and other commercial projects',
-    },
-    { name: 'Portraits', description: 'Portraits of people in my life' },
-    { name: 'Food', description: 'Delicious delicacies' },
-    {
-      name: 'Landscape',
-      description: 'Fields, farmhouses, waterfalls, and the beauty of nature',
-    },
+      name: 'commercial',description: 'Photos of grocery stores, food trucks, and other commercial projects',},
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
       <Nav
@@ -29,7 +22,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <Gallery></Gallery>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
     </div>
